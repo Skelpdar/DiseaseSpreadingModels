64a7944f1b2907485dbe951c992960fcea30cf0b
Erik Wallin <skelpdar@gmail.com>
2020-09-14 00:50:19 +0200

    Added rungekutta with a SIS model example

---
+++ inc/rungeKutta.h
@@ -0,0 +1,48 @@
+#include <vector>
+#include <Eigen/Dense>
+
+#ifndef RUNGE_KUTTA_H
+#define RUNGE_KUTTA_H
+
+using Eigen::Vector2d;
+
+template <typename VectorType, typename Function>
+VectorType rungeKuttaStep(VectorType yn, Function func, double h, double tn){
+    VectorType k1 = h*func(tn, yn);
+    VectorType k2 = h*func(tn+h/2, yn+k1/2);
+    VectorType k3 = h*func(tn+h/2, yn+k2/2);
+    VectorType k4 = h*func(tn+h, yn+k3);
+
+    return yn+k1/6+k2/3+k3/3+k4/6;
+}
+
+template <typename VectorType, typename Function>
+VectorType rungeKutta(VectorType y0, Function func, double h, int iterations, std::vector<VectorType>& data, std::vector<VectorType>& error){
+    VectorType yn = y0;
+    double t = 0;
+    for(int i=0; i<iterations; i++){
+        data.push_back(yn);
+
+        yn = rungeKuttaStep(yn, func, h, t);
+
+        t += h;
+    }
+    
+    std::vector<VectorType> halfSteps;
+    yn = y0;
+    t = 0;
+    for(int i=0; i<iterations*2; i++){
+        halfSteps.push_back(yn);
+
+        yn = rungeKuttaStep(yn, func, h/2, t);
+
+        t += h/2;
+    }
+    
+    for(int i=0; i <iterations; i++){
+        error.push_back((halfSteps[2*i]-data[i])/(32-1));
+    }
+    
+}
+
+#endif
--- src/ode.cpp
+++ src/ode.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
+#include <fstream>
 #include <Eigen/Dense>
+#include <vector>
+
+#include <rungeKutta.h>
 
 using Eigen::MatrixXd;
+using Eigen::Vector2d;
+
+using namespace Eigen;
+
+double beta = 0.07;
+double alpha = 0.05;
+
+Vector2d SIS(double tn, Vector2d y){
+    Vector2d v;
+
+    v(0) = -beta*y(0,0)*y(1,0)/(y(0,0)+y(1,0))+alpha*v(1,0);
+    v(1) = beta*y(0,0)*y(1,0)/(y(0,0)+y(1,0))-alpha*v(1,0);
+
+    return v;
+
+}
 
 int main(){
-    std::cout << "Hello world" << std::endl;
-    MatrixXd m(2,2);
+    Vector2d y0;
+    y0(0) = 70;
+    y0(1) = 30;
 
-	std::cout << m << std::endl;
+    std::vector<Vector2d> data;
+    std::vector<Vector2d> error;
+
+    rungeKutta(y0, SIS, 10, 100, data, error);
+
+    std::ofstream SISFile;
+    SISFile.open("SISSolution.txt");
+
+    for(int i=0; i<data.size(); i++){
+        SISFile << data[i] << std::endl;
+        SISFile << error[i] << std::endl;
+    }
+
+    SISFile.close();
 
     return 0;
 }
